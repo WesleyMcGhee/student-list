4c9d108f4991606fb5761bba360a3d2550ad7c98
WesleyMcGhee <wesmcghee6372@gmail.com>
2021-07-20 10:43:25 -0500

    added rollbar errors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const rollbar = new Rollbar({
 
 const app = express();
 app.use(express.json());
-
+app.use(rollbar.errorHandler());
 let studentList = [];
 
 app.get("/", (req, res) => {
@@ -31,15 +31,15 @@ app.post("/api/student", (req, res) => {
   if (index === -1 && name !== "") {
     studentList.push(name);
     // add rollbar log here
-
+    rollbar.log('Student added succesfully', {author: 'Wesley', type: 'Manual'})
     res.status(200).send(studentList);
   } else if (name === "") {
     // add a rollbar error here
-
+    rollbar.error('no name givin');
     res.status(400).send({ error: "no name was provided" });
   } else {
     // add a rollbar error here too
-
+    rollbar.error('student already exists');
     res.status(400).send({ error: "that student already exists" });
   }
 });
